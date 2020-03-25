50f3a703c8f62cadf2cefddae2fd2c0b87460e3b
张鑫 <zxin088@gmail.com>
2020-03-25 17:07:09 +0800

    新增并配置图片选择插件

---
+++ jsbridge-plugins-n22/src/main/java/com/ospoon/app/sunlife/jsbridge_plugins_n22/Constants.java
@@ -0,0 +1,8 @@
+package com.ospoon.app.sunlife.jsbridge_plugins_n22;
+
+
+import java.security.PublicKey;
+
+public class Constants {
+  public final static int OPEN_IMAGE_SELECT_REQUEST_CODE = 201;
+}
